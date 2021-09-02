426666f27e37c7127af5ec6e7d28f7439656590f
100lv <stolev@gmail.com>
2021-09-02 15:46:01 +0300

    Fuxed

--- chia_madmax_start.sh
+++ chia_madmax_start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo /home/chpt/chia-plotter/chia_plot -n -1  -r 8 -t /chia/tmpdir/ -2 /chia/tmpdir2/ -d /chia/dest/ -f b0c6d1a29116385670577f62d697a62fb9a810f0c4ee3780a2fc29b905bfc7a003e053341bc905a8bb173f246aa37c81  -c xch1j3356j56mflh4jdqxppu7gxrq2v85xnklvj9n29sv9mlfsl76umsnswcen
+sudo /home/chpt/chia-plotter/build/chia_plot -n -1  -r 8 -t /chia/tmpdir/ -2 /chia/tmpdir2/ -d /chia/dest/ -f b0c6d1a29116385670577f62d697a62fb9a810f0c4ee3780a2fc29b905bfc7a003e053341bc905a8bb173f246aa37c81  -c xch1j3356j56mflh4jdqxppu7gxrq2v85xnklvj9n29sv9mlfsl76umsnswcen
